51539e1fbf791820e1e48f2c891ba4a8048a98e6
Emily Lafontaine <LafoEmil239@GOTVDSB.CA>
2021-02-05 12:26:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EEEEE");
+            Console.WriteLine("E");
+            Console.WriteLine("E");
+            Console.WriteLine("EEE");
+            Console.WriteLine("E");
+            Console.WriteLine("E");
+            Console.WriteLine("EEEEE");
+
+            Console.WriteLine("L");
+            Console.WriteLine("L");
+            Console.WriteLine("L");
+            Console.WriteLine("L");
+            Console.WriteLine("L");
+            Console.WriteLine("L");
+            Console.WriteLine("LLLLL");
+
+            string firstName = "Emily";
+            string favMovie = "The Matrix";
+            Console.WriteLine($"Hello, my name is {firstName} and my favourite movie is {favMovie}");
+            favMovie = favMovie.ToUpper();
+            Console.WriteLine(favMovie);
+            Console.WriteLine(favMovie.Contains("THE"));
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+            Console.WriteLine(favMovie);
+        }
+    }
+}
